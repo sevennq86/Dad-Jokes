893678519f2d596be2095bd426026286406e173d
sevennq86 <brandonneves@brandons-mbp.lan>
2023-02-09 11:03:01 -0500

    api call setup

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').trigger('focus')
 })
+
+// variable to grab the button
+const button = document.querySelector("#jokeButton")
+
+// event listener function
+button.addEventListener("click", function () {
+  makeApiRequest();
+});
+
+async function makeApiRequest() {
+  const response = await fetch('https://icanhazdadjoke.com/', {
+    headers: {
+      "Accept": "application/json"
+    }
+  })
+
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+
+  
+  // const data = await response.json();
+
+  // console.log(data);
+}
